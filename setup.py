b1c76007fa4f9f3c8154a171a294262e93a044d7
piotr <nwg.piotr@gmail.com>
2019-07-01 22:27:59 +0200

    v1.1.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(f_name):
 
 setup(
     name='azote',
-    version='1.1.0',
+    version='1.1.2',
     description='Wallpaper manager for Sway, i3 and some other WMs',
     packages=['azote'],
     include_package_data=True,
