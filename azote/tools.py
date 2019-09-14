7131d255f61e4b9d183e0293047d49c6647bf53c
piotr <nwg.piotr@gmail.com>
2019-09-15 00:45:28 +0200

    way of checking if we're running sway altered to avoid crash on qtile

--- azote/tools.py
+++ azote/tools.py
@@ -55,7 +55,7 @@ def str_to_bool(s):
 def check_displays():
     # Sway or not Sway? If so, the swaymsg command should return exit code 0
     result = subprocess.run(['swaymsg', '-t', 'get_outputs'], stdout=subprocess.DEVNULL)
-    common.sway = result == 0
+    common.sway = result.returncode == 0
     # ask for the wm name (just for logging: maybe we should give up on it to drop the wmctrl dependency?)
     wm = subprocess.check_output("wmctrl -m | awk '/Name/{print $2}'", shell=True).decode("utf-8").strip()
     if common.sway:
