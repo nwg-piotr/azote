3629b51b4ca27ef86c770c35c86ad3fc6fd5abcc
piotr <nwg.piotr@gmail.com>
2023-06-19 01:10:41 +0200

    bump to 1.12.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(f_name):
 
 setup(
     name='azote',
-    version='1.12.0',
+    version='1.12.1',
     description='Wallpaper manager for sway and some other WMs',
     packages=find_packages(),
     include_package_data=True,
