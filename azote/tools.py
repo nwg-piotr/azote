60f5db1edd2fab1f843ebe5acfcaa2cb52f99710
piotr <nwg.piotr@gmail.com>
2024-05-03 01:37:13 +0200

    get ready for Hyprland socket files moved

--- azote/tools.py
+++ azote/tools.py
@@ -115,9 +115,14 @@ def check_displays():
 
     elif os.getenv("HYPRLAND_INSTANCE_SIGNATURE"):
         print("Running on Hyprland")
+        # /tmp/hypr moved to $XDG_RUNTIME_DIR/hypr in #5788
+        xdg_runtime_dir = os.getenv("XDG_RUNTIME_DIR")
+        hypr_dir = f"{xdg_runtime_dir}/hypr" if xdg_runtime_dir and os.path.isdir(
+            f"{xdg_runtime_dir}/hypr") else "/tmp/hypr"
+
         import socket
         s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-        s.connect("/tmp/hypr/{}/.socket.sock".format(os.getenv("HYPRLAND_INSTANCE_SIGNATURE")))
+        s.connect("{}/{}/.socket.sock".format(hypr_dir, os.getenv("HYPRLAND_INSTANCE_SIGNATURE")))
 
         s.send("j/monitors".encode("utf-8"))
         output = s.recv(20480).decode('utf-8')
