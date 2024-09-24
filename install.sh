f7877a5e4ebd7ad4e07292f3dfa689565a314225
piotr <nwg.piotr@gmail.com>
2024-09-25 01:57:27 +0200

    add install.sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+install -Dm 644 -t /usr/share/pixmaps dist/azote.svg
+install -Dm 644 -t /usr/share/azote dist/indicator*.png
+install -Dm 644 -t /usr/share/applications dist/azote.desktop
+install -Dm 644 -t /usr/share/doc/azote README.md
+
+python -m build --wheel --no-isolation
+python -m installer dist/*.whl
