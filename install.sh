b04e2a67a17ec2f273fb7b9642361b96f1e45265
piotr <nwg.piotr@gmail.com>
2024-09-25 02:22:36 +0200

    remove /usr/bin/azote

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+rm -f /usr/bin/azote
+
 install -Dm 644 -t /usr/share/pixmaps dist/azote.svg
 install -Dm 644 -t /usr/share/azote dist/indicator*.png
 install -Dm 644 -t /usr/share/applications dist/azote.desktop
